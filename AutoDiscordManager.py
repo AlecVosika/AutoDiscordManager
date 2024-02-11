f3ee670adb13ca5ba5e07cb9959913bdd62a08ba
AlecVosika <alecvosika@comcast.net>
2024-02-11 17:39:45 -0600

    Added device monitoring and Discord control Added Config.ini

--- AutoDiscordManager.py
+++ AutoDiscordManager.py
@@ -1,123 +1,137 @@
+import logging
 from time import sleep
 import psutil
 from scapy.all import ARP, Ether, srp
 import subprocess
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 # Constants
 ARP_TIMEOUT = 3
 CHECK_INTERVAL = 5
-MAX_CHECKS = 3
-DEVICE_IP = '192.168.1.24'  # Replace with the IP address of the device you want to monitor
-DISCORD_FILE_PATH = 'C:\\Users\\Alecv\\AppData\\Local\\Discord\\app-1.0.9032\\Discord.exe'  # Replace with the file path of Discord on your system
+MAX_FAILED_CHECKS = 5
+DEVICE_IP = config.get('Device', 'device_ip')
+DISCORD_FILE_PATH = config.get('Discord', 'discord_file_path')
 
-def get_mac(ip):
+class DeviceMonitor:
     """
-    Get the MAC address of a given IP address using ARP.
+    A class for monitoring the status of a device.
 
-    Args:
-        ip (str): The IP address to get the MAC address for.
+    Attributes:
+        device_ip (str): The IP address of the device to monitor.
+        discord_file_path (str): The file path of the Discord file.
 
-    Returns:
-        str: The MAC address of the given IP address.
+    Methods:
+        get_mac(ip): Get the MAC address of a given IP address using ARP.
+        check_device_status(): Check the status of a device by its IP address.
     """
-    # Create an ARP request packet
-    arp = ARP(pdst=ip)
-    ether = Ether(dst="ff:ff:ff:ff:ff:ff")
-    packet = ether/arp
 
-    # Send the packet and receive the response
-    result = srp(packet, timeout=ARP_TIMEOUT, verbose=0)[0]
+    def __init__(self, device_ip, discord_file_path):
+        self.device_ip = device_ip
+        self.discord_file_path = discord_file_path
 
-    # Extract the MAC address from the response
-    return result[0][1].hwsrc
+    def get_mac(self, ip):
+        """
+        Get the MAC address of a given IP address using ARP.
 
-def check_device_status(device_ip):
+        Args:
+            ip (str): The IP address to get the MAC address for.
+
+        Returns:
+            str: The MAC address of the given IP address.
+        """
+        arp = ARP(pdst=ip)
+        ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+        packet = ether / arp
+        result = srp(packet, timeout=ARP_TIMEOUT, verbose=0)[0]
+        return result[0][1].hwsrc
+
+    def check_device_status(self):
+            """
+            Checks the status of a device by retrieving its MAC address using the device's IP address.
+
+            Returns:
+                bool: True if the device is found and its MAC address is retrieved successfully, False otherwise.
+            """
+            try:
+                self.get_mac(self.device_ip)
+                return True
+            except IndexError:
+                logging.error(f"Device with IP {self.device_ip} not found.")
+                return False
+            except Exception as e:
+                logging.error(f"An error occurred while checking device status: {str(e)}")
+                return False
+
+class DiscordController:
     """
-    Check the status of a device by its IP address.
-
-    Args:
-        device_ip (str): The IP address of the device.
-
-    Returns:
-        bool: True if the device is found, False otherwise.
+    A class that provides methods to interact with Discord application.
     """
-    try:
-        # Get the MAC address of the device
-        get_mac(device_ip)
-        return True
-    except IndexError:
-        # Device not found, return False
+
+    @staticmethod
+    def is_discord_open():
+        """
+        Check if Discord application is currently open.
+
+        Returns:
+            bool: True if Discord is open, False otherwise.
+        """
+        for proc in psutil.process_iter(['name']):
+            if proc.info['name'] == 'Discord.exe':
+                return True
         return False
 
-def is_discord_open():
+    @staticmethod
+    def close_discord():
+        """
+        Close the Discord application if it is currently open.
+        """
+        for proc in psutil.process_iter(['name']):
+            if proc.info['name'] == 'Discord.exe':
+                proc.kill()
+
+    @staticmethod
+    def open_discord(file_path):
+        """
+        Open the Discord application using the specified file path.
+
+        Args:
+            file_path (str): The path to the Discord executable file.
+        """
+        subprocess.Popen(file_path)
+
+def main():
     """
-    Check if Discord application is currently running.
+    Main function that monitors the device status and controls Discord.
 
-    Returns:
-        bool: True if Discord is running, False otherwise.
-    """
-    # Iterate over all running processes
-    for proc in psutil.process_iter(['name']):
-        # Check if the process is Discord.exe
-        if proc.info['name'] == 'Discord.exe':
-            return True
-    return False
-
-def close_discord():
-    """
-    Closes the Discord application if it is currently running.
-
-    This function iterates over all running processes and checks if the process
-    name is 'Discord.exe'. If a running process with the name 'Discord.exe' is found,
-    it is terminated.
-
-    Note: This function requires the `psutil` module to be installed.
-
-    Returns:
-        None
-    """
-    # Iterate over all running processes
-    for proc in psutil.process_iter(['name']):
-        # Check if the process is Discord.exe
-        if proc.info['name'] == 'Discord.exe':
-            # Kill the process
-            proc.kill()
-
-import subprocess
-
-def open_discord(file_path):
-    """
-    Opens Discord using the subprocess module with the specified file path.
+    It creates instances of DeviceMonitor and DiscordController classes.
+    It continuously checks the device status and performs actions based on the status.
+    If the device status fails for a certain number of checks, it closes Discord.
+    If Discord is not open, it opens Discord.
 
     Parameters:
-    file_path (str): The path to the Discord executable file.
+    None
 
     Returns:
     None
     """
-    subprocess.Popen(file_path)
+    device_monitor = DeviceMonitor(DEVICE_IP, DISCORD_FILE_PATH)
+    discord_controller = DiscordController()
 
-def main():
-    """
-    Main function that continuously checks the device status and closes 
-    Discord if the device is not found and opens discord if it is found.
-    """
     while True:
         checks = 0
         sleep(CHECK_INTERVAL)
-        while checks < MAX_CHECKS:
-            if not check_device_status(DEVICE_IP):
-                # Device not found, increment the number of checks
+        while checks < MAX_FAILED_CHECKS:
+            if not device_monitor.check_device_status():
                 checks += 1
-                if checks == MAX_CHECKS:
-                    # If the device is still not found after MAX_CHECKS, close Discord
-                    close_discord()
+                if checks == MAX_FAILED_CHECKS:
+                    discord_controller.close_discord()
             else:
-                # Device found, reset the number of checks
                 checks = 0
-                if not is_discord_open():
-                    # If Discord is not open, open it
-                    open_discord(DISCORD_FILE_PATH)
+                if not discord_controller.is_discord_open():
+                    discord_controller.open_discord(DISCORD_FILE_PATH)
 
 if __name__ == "__main__":
     main()
